db194b83229bfb71dc0debfc8e299cdd7b0b1a06
William <gallamine@gmail.com>
2022-12-20 10:32:18 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,10 +14,10 @@ export default function Home() {
             <li><a href="https://youtu.be/O4hEbNKEwp0">Family Trip to Main 2021</a></li>
             <li><a href="https://youtu.be/imSFVfQeJU4">Arizona 2020</a></li>
             <li><a href="https://youtu.be/XDq8rHY9_Pc">OBX 2020</a></li>
-          <li><a href="https://youtu.be/SZSxPAUwb3c">Uwharrie 20202</a></li>
-          <li><A href="https://youtu.be/DQWaLuglPUY">Winter Camping 20202</a></li>
-          <li><a href="https://www.youtube.com/watch?v=bfPwRzp93N4">Utah 2020</a></li>
-          <li><a href="https://youtu.be/5AZLsoM5Y4A">OBX Trip 2019</a></li>
+            <li><a href="https://youtu.be/SZSxPAUwb3c">Uwharrie 20202</a></li>
+            <li><a href="https://youtu.be/DQWaLuglPUY">Winter Camping 20202</a></li>
+            <li><a href="https://www.youtube.com/watch?v=bfPwRzp93N4">Utah 2020</a></li>
+            <li><a href="https://youtu.be/5AZLsoM5Y4A">OBX Trip 2019</a></li>
             
         </ul>
       </p>
