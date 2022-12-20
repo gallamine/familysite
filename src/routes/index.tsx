ee821958ffbaa526c7f0cf96431821f2a51bc6fd
William <gallamine@gmail.com>
2022-12-20 10:30:21 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,22 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Cox Family</Title>
+      <h1>Hello!</h1>
       <Counter />
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        Trip recaps:
+        <ul>
+          <li>South Africa 2022 - coming soon</li>
+            <li><a href="https://youtu.be/O4hEbNKEwp0">Family Trip to Main 2021</a></li>
+            <li><a href="https://youtu.be/imSFVfQeJU4">Arizona 2020</a></li>
+            <li><a href="https://youtu.be/XDq8rHY9_Pc">OBX 2020</a></li>
+          <li><a href="https://youtu.be/SZSxPAUwb3c">Uwharrie 20202</a></li>
+          <li><A href="https://youtu.be/DQWaLuglPUY">Winter Camping 20202</a></li>
+          <li><a href="https://www.youtube.com/watch?v=bfPwRzp93N4">Utah 2020</a></li>
+          <li><a href="https://youtu.be/5AZLsoM5Y4A">OBX Trip 2019</a></li>
+            
+        </ul>
       </p>
     </main>
   );
