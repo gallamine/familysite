f5bf7e0bebbe93642051965d7bace9d7f6f684bc
William <gallamine@gmail.com>
2022-12-20 10:33:39 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,6 +12,7 @@ export default function Home() {
         <ul>
           <li>South Africa 2022 - coming soon</li>
             <li><a href="https://youtu.be/O4hEbNKEwp0">Family Trip to Main 2021</a></li>
+            <li><a href="">Roanoke River Trip -- coming soon</a></li>
             <li><a href="https://youtu.be/imSFVfQeJU4">Arizona 2020</a></li>
             <li><a href="https://youtu.be/XDq8rHY9_Pc">OBX 2020</a></li>
             <li><a href="https://youtu.be/SZSxPAUwb3c">Uwharrie 20202</a></li>
