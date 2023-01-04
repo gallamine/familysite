8f4bd2600763e02f4eb449a1ec2af8cd33c9b995
William <gallamine@gmail.com>
2023-01-04 14:06:40 -0500

    Update index.tsx
    Fix dates

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,8 +15,8 @@ export default function Home() {
             <li><a href="">Roanoke River Trip -- coming soon</a></li>
             <li><a href="https://youtu.be/imSFVfQeJU4">Arizona 2020</a></li>
             <li><a href="https://youtu.be/XDq8rHY9_Pc">OBX 2020</a></li>
-            <li><a href="https://youtu.be/SZSxPAUwb3c">Uwharrie 20202</a></li>
-            <li><a href="https://youtu.be/DQWaLuglPUY">Winter Camping 20202</a></li>
+            <li><a href="https://youtu.be/SZSxPAUwb3c">Uwharrie 2020</a></li>
+            <li><a href="https://youtu.be/DQWaLuglPUY">Winter Camping 2020</a></li>
             <li><a href="https://www.youtube.com/watch?v=bfPwRzp93N4">Utah 2020</a></li>
             <li><a href="https://youtu.be/5AZLsoM5Y4A">OBX Trip 2019</a></li>
             
