b09698bb9b71896e5796d276dd368a467975bea3
William <gallamine@gmail.com>
2026-06-25 09:39:14 -0400

    Fix crypto property error in Netlify runtime

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,8 +1,17 @@
-import {
-  createHandler,
-  renderAsync,
-  StartServer,
-} from "solid-start/entry-server";
+const cryptoDescriptor = Object.getOwnPropertyDescriptor(globalThis, "crypto");
+
+if (cryptoDescriptor?.get && !cryptoDescriptor.set && cryptoDescriptor.configurable) {
+  Object.defineProperty(globalThis, "crypto", {
+    configurable: true,
+    enumerable: cryptoDescriptor.enumerable,
+    value: cryptoDescriptor.get.call(globalThis),
+    writable: true,
+  });
+}
+
+const { createHandler, renderAsync, StartServer } = await import(
+  "solid-start/entry-server"
+);
 
 export default createHandler(
   renderAsync((event) => <StartServer event={event} />)
